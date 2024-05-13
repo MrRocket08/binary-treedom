2a1f6c42cbc407db02d31b67b15cba9eecfeaafd
MrRocket08 <122652107+MrRocket08@users.noreply.github.com>
2024-05-13 13:21:36 -0400

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // fields
+    private int health;
+    private int stamina;
+    private int speed;
+    private int defense;
+    private Item[] inventory;
+
+    // class methods
+    void FixedUpdate()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void Attack() { }
+
+    // accessor methods
+
+    // mutator methods
 }
