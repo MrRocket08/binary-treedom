c13cef6d271d61a14866e31868df0b8e0b847264
MrRocket08 <122652107+MrRocket08@users.noreply.github.com>
2024-05-13 19:44:56 -0400

    class structuring (4)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,9 @@ public class Player : MonoBehaviour
 {
     // fields
     private int health;
+    private float healthRegen;
     private int stamina;
+    private float staminaRegen;
     private int speed;
     private int defense;
     private Item[] inventory;
@@ -20,6 +22,13 @@ public class Player : MonoBehaviour
     public void Attack() { }
 
     // accessor methods
+    public int getHealth() { return health; }
+    public float getHealthRegen() { return healthRegen; }
+    public int getStamina() { return stamina; }
+    public float getStaminaRegen() { return staminaRegen; }
+    public int getSpeed() { return speed; }
+    public int getDefense() { return defense; }
+    public Item[] getInventory() { return inventory; }
 
     // mutator methods
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,15 +8,17 @@ public class Weapon : Item
     private int damage;
     private int damageRange;
     private int useSpeed;
+    private float staminaUse;
     private string name;
     private string type;
 
     // class methods
-    public Weapon(string _name, string _type, int _damage, int _damageRange, int _useSpeed)
+    public Weapon(string _name, string _type, int _damage, int _damageRange, int _useSpeed, float _staminaUse)
     {
         damage = _damage;
         damageRange = _damageRange;
         useSpeed = _useSpeed;
+        staminaUse = _staminaUse;
         name = _name;
         type = _type;
     }
