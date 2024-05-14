7cd3df824dc433e0ee2d1a0ad0a0323e9ca98c32
varakelian7 <vartan.arakelian7@gmail.com>
2024-05-14 13:28:15 -0400

    removed some lines

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -10,24 +10,16 @@ public class PlayerMovement : MonoBehaviour
     float horizontalMove = 0f;
     bool jump = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal"); // value btwn -1 and 1 from keyboard
-        if (Input.GetButtonDown("Jump")){
-            jump = true;
-        }
+       
     }
 
     void FixedUpdate(){
         // move character
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
-        jump = false;
     }
 }
