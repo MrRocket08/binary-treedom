83fe98903d6f13d4b2e7ffa6113a2b803c49a456
varakelian7 <vartan.arakelian7@gmail.com>
2024-05-14 13:20:54 -0400

    Update PlayerMovement.cs

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,6 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
     
     public CharacterController2D controller;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +19,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalMove = Input.GetAxisRaw("Horizontal"); // value btwn -1 and 1 from keyboard
+        if (Input.GetButtonDown("Jump")){
+            jump = true;
+        }
+    }
+
+    void FixedUpdate(){
+        // move character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
