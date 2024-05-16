0134dd9a1d8880726e3dc4ffc8962fe052ee0c39
MrRocket08 <122652107+MrRocket08@users.noreply.github.com>
2024-05-16 11:18:21 -0400

    built gamemaster and enemy class

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    // fields
+    protected float health;
+    protected int defense;
+    protected int speed;
+    protected int damage;
+
+    // class methods
+    protected Player Attack()
+    { }
+}
---
+++ Assets/Scripts/GameMasterScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMasterScript : MonoBehaviour
+{
+    public Vector2[] spawnLocations;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnLocations = new Vector2[1];
+
+        for (int i = 0; i < spawnLocations.Length; i++)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
