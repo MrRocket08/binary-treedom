dec2b3d7c6cf85128ebf6a766720e8f7893a5585
anniezhu08 <aannnniieezhu@gmail.com>
2024-05-16 11:03:10 -0400

    updated playermovement to temporary dash ability

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,26 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
 
+
     public Rigidbody2D rb;
-    public Player player;
     public float moveSpeed = 40f;
     public Animator animator;
     Vector2 movement;
 
-    //Dash Ability Class
-    public Dash dashAbility;
 
-    void Start()
-    {
-        player = this.GameObject.GetComponent<Player>();
-    }
+    //Dashing Field Variables
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 48f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
+
 
     // Update is called once per frame
     void Update()
     {
+
+
+        //prevents the player from dashing and walking at the same time
+        if(isDashing)
+        {
+            return;
+        }
+
+
         // input
         movement.x = Input.GetAxisRaw("Horizontal"); // value in {-1, 0, 1} from keyboard
         movement.y = Input.GetAxisRaw("Vertical");
@@ -52,23 +63,52 @@ public class PlayerMovement : MonoBehaviour
             animator.SetTrigger("RunForward");
         }
 
-        if (Input.GetMouseButtonDown(1) && player.getStamina() > 2)
-        {
-            Vector3 mousePositionScreen = Input.mousePosition;
-            Vector3 mousePositionWorld = Camera.main.ScreenToWorldPoint(mousePositionScreen);
 
-            // Check if the current ability is a dash ability
-            //if (dashAbility != null)
-            //{
-            //PerformDash(mousePositionWorld);
-            //}
+        //dashing routine
+        if(Input.GetMouseButtonDown(1) && canDash){
+            StartCoroutine(Dash());
         }
     }
 
+
     void FixedUpdate()
     {
-
+        //preventing player from moving and dashing at the same time
+        if (isDashing)
+        {
+            return;
+        }
         // movement
         rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
     }
+
+
+    //Dashing Co-Routine
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+       
+         // Calculate direction from player to mouse position
+        Vector3 mousePositionScreen = Input.mousePosition;
+        Vector3 mousePositionWorld = Camera.main.ScreenToWorldPoint(mousePositionScreen);
+        Vector2 dashDirection = (mousePositionWorld - transform.position).normalized;
+
+
+        // Apply force for dashing
+        rb.velocity = dashDirection * dashingPower;
+
+
+        yield return new WaitForSeconds(dashingTime);
+
+
+        rb.velocity = Vector2.zero;
+        isDashing = false;
+
+
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+    }
 }
+
+
