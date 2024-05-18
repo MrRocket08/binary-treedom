cb732dd76aa3aadcc7fca2a431990750427a12dc
varakelian7 <vartan.arakelian7@gmail.com>
2024-05-18 17:22:40 -0400

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -96,5 +96,14 @@ public class Player : MonoBehaviour
     public void setSpeed(int _speed) { speed = _speed; }
     public void setDefense(int _defense) { defense = _defense; }
     public void setInventory(int index, Item i) { inventory[index] = i; }
-    public void addToInventory(Item i){/* decide best index to add item to*/}
+    public void addToInventory(Item i)
+    {
+        /* decide best index to add item to*/
+        if (inventory[0] == null){setInventory(0, i);}
+        else if (inventory[1] == null){setInventory(1, i);}
+        else if (inventory[2] == null){setInventory(2, i);}
+        else if (inventory[3] == null){setInventory(3, i);}
+        else (inventory[4] == null){setInventory(4, i);}
+    }
+    public void removeItem(int index){inventory[index] = null;}
 }
