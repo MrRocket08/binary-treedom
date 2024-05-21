ee1668a4287eaab718747174eae332f75caa09d2
MrRocket08 <122652107+MrRocket08@users.noreply.github.com>
2024-05-21 14:57:40 -0400

    fixed tiling

---
+++ Assets/Scripts/TreeSlapper.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeSlapper : Weapon
+{
+
+    public Transform currentTransform;
+
+
+    public TreeSlapper(string nombre) : base(nombre, "TreeSlapper", 5, 3, 2, 20, 20) { currentTransform = currentTransform; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            // animation of sword swinging
+            Collider2D[] hits = Physics2D.OverlapBoxAll(currentTransform.position, new Vector2(1, 1), currentTransform.rotation.eulerAngles.x);
+            if (hits != null)
+            {
+                foreach (Collider2D Col in hits)
+                {
+                    if (Col.gameObject.CompareTag("Enemy"))
+                    {
+                        Hit(Col.gameObject.GetComponent<Enemy>());
+                        Debug.Log("Hit");
+                    }
+                }
+            }
+        }
+    }
+
+}
